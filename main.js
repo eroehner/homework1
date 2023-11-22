f5d818f5237b36e6b3557ae16bc0e59b2aaeee7b
eroehner <e.roehner@icloud.com>
2023-11-22 23:37:22 +0100

    new main

--- main.js
+++ main.js
@@ -1,192 +1,77 @@
-import './style.css'
-/*//Declaration
-var studlyCapVar;
-var properCamelCase;
-var TitleCaseOver
-var a = 2.75;
-var b = 25.5;
-var c = 10.9;
-var d;
-var remainder;
-var Vorname = "Elias";
-var Nachname = "Röhner";
-var vollerName;
-var firstNameLength=0;
-var favColor = "Blau";
-var favColorLength;
-var firstLetterOfFavColor;
-var lastLetterOfFavColor;
-function wordBlanks(myNoun, myAdjective, myVerb, myAdverb){
-var mySentence = " ";
-mySentence = "Der " + myAdjective + myNoun + myVerb + myAdverb + " zum LIDL " ;
-return mySentence;
-}
-var myArray = ["Ready Player One", "Avatar", "Interstellar", "Dune"];
-var ourArray
-var numberArray = [[1,2,3],[4,5,6],[7,8,9], [[10,11,12],13,14]];
-var myArrayNew ;
-var age;
+//IMPORT MODULES
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-//Assignment
-studlyCapVar = 16;
-properCamelCase = "A String";
-TitleCaseOver = 9000;
-a+=5;
-b=b+5;
-c=c+" croissants s'il vous plait"
-d=" <3"
-remainder = 11%3;
-vollerName=Vorname;
-vollerName+=" ";
-vollerName+=Nachname;
-firstNameLength=Vorname.length;
-firstLetterOfFavColor = favColor[2];
-favColorLength = favColor.length;
-lastLetterOfFavColor = favColor[favColorLength-1];
-favColor= "Lila";
-myArray[0] = "Forrest Gump";
-myArray.push(["Matrix"]);
-myArray.pop();
-age = 15;
-
-//Condition Declaration
-var newText = (age<18) ? "Can't buy alcohol!" : "Can buy alcohol";
-
-var number = 2;
-
-switch (number){
-case(0):
-console.log("Sunday")
-break;
-case(1):
-console.log("Monday")
-break;
-case(2):
-console.log("Tuesday")
-break;
-case(3):
-console.log("Wednesday")
-break;
-case(4):
-console.log("Thursday")
-break;
-case(5):
-console.log("Friday")
-break;
-case(0):
-console.log("Saturday")
-break;
-};
-
-for (let i = 0; i <= 5; i++) {
-    console.log(i);
-}
-
-var a=1;
-while (a <= 10) {
-    a++;
-    console.log("Nummer"+a);
-    if(a==5){break;}
-    
-}
+//VARIABLES
+let width = window.innerWidth;
+let height = window.innerHeight;
+var geometries = [];
+let materials = [];
+var cubes = [];
+//CHALLENGE: Add an array to store multiple cubes.
  
-
-//console
-console.log(studlyCapVar);
-console.log(a);
-console.log(b);
-console.log(c+d);
-console.log(remainder);
-console.log(vollerName);
-console.log("Der Vorname besteht aus "+firstNameLength+ " Buchstaben.");
-console.log(firstLetterOfFavColor);
-console.log(lastLetterOfFavColor);
-console.log(favColor);
-console.log(wordBlanks( " Hund", "crazy ass", " flitze ", "turboschnell"));
-console.log(numberArray[2][1]);
-console.log(myArray);
-console.log(myArrayNew);
-console.log(newText);
-
-const myObject = {
-name: "Zac",
-age: 14,
-speak: function(){
-console.log("Hi! I am " + this.name)
-},
-};
-
-console.log(myObject);
-myObject.speak();
-
-const myObject2 = {
-    name:"Elias",
-    age: 22,
-    speak: function(){
-        console.log("Hi! My age is " + this.age);
-    }
+//CREATE SCENE AND CAMERA
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 45, width / height, 0.1, 100);
+camera.position.set(0, 0, 20);
+ 
+//CREATE GEOMETRY AND ADD TO THE SCENE
+for(let i = 1; i <= 4; i++){
+    geometries[i] = new THREE.BoxGeometry( i, i, i);
+    materials [i] = new THREE.MeshBasicMaterial( { color: i/100 * 0xffffff } );
+    cubes [i]= new THREE.Mesh( geometries[i], materials[i] );
+    cubes[i].position.set(1, i*i, 1);
 }
+cubes.forEach(function(element, index){
+    scene.add(element);
+})
 
-myObject2.speak();
-
-function createPerson(_name, _age){
-    return{
-        name:_name,
-        age:_age,
-        speak:function(){
-            console.log("Hi1 My name is " + this.name);
-        }
-    }
-};
-
-let son1 = createPerson("Jaden", 7);
-son1.speak();
-
-let son2 =createPerson("Zac", 7);
-son2.speak();
-
-for (let i=0;i<5; i++){
-    let son = createPerson("son" + i, i+2);
-    son.speak();
-}
-
-class Person {
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
-    introduce(){
-            console.log("Hi! My name is " + this.name + " and I am " + this.age + " years old.")
-    }
-}
-
-const julian = new Person ("Julian", 21);
-julian.introduce();
-
-class Student extends Person{
-    constructor(name, age, major){
-        super(name, age)
-        this.major = major;
-    }
-    introduceWithMajor(){
-        
-        console.log(`Hi my name is ${this.name}, I am ${this.age} years old and I am studying ${this.major}`)
-    }
-}
-
-const studentJulian = new Student ("Julian", 21, "architecture");
-studentJulian.introduceWithMajor;
-
-class Person2 {
-    introduce(name, age){
-        console.log(`Hi! My name is ${name} an I am ${age} years old.`);
-    }
-    introduce(name, age, study){
-        console.log(`Hi! my name is ${name}, I am ${age} years old and I am studying ${study}.`);
-    }
-}
-
-const julian2 = new Person2;
-julian2.introduce("Julian", 21);
-julian2.introduce("Julian", 21, "architeture");
+/*const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0xbcc8ec } );
+const cube = new THREE.Mesh( geometry, material );
+cube.position.set(1,5,0)
+scene.add( cube );
 */
+//CHALLENGE: Create multiple cubes using loops and set different positions for each cube. 
+// For each cube, add them to the array before adding them to the scene. 
+ 
+// RESPONSIVE
+ 
+//CREATE A RENDERER
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+const container = document.querySelector('#threejs-container')
+container.append(renderer.domElement)
+//CREATE MOUSE CONTROL
+const controls = new OrbitControls( camera, renderer.domElement );
+ 
+//ANIMATE AND RENDER
+function animate() {
+requestAnimationFrame( animate );
+controls.update();
+
+cubes.forEach(function(element, index){
+    element.rotation.x += 0.01 * index;
+    element.rotation.y += 0.02 * index;
+})
+
+
+
+
+renderer.render( scene, camera );
+}
+
+
+animate();
+
+window.addEventListener('resize', () => {
+width = window.innerWidth
+height = window.innerHeight
+camera.aspect = width / height
+camera.updateProjectionMatrix()
+renderer.setSize(width, height)
+renderer.render(scene, camera)
+})
+//CHALLENGE: Inside the function animate( ), iterate through the array to access each cube using ForEach. Set different rotation (i.e. different speeds, different axes) for each cube.
